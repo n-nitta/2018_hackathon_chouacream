5529c348c7175405e7bfe513bf33d379a5a64dca
ryusuke-nakagawa <ryu97161@gmail.com>
2018-09-18 16:19:06 +0900

    メイン

--- chouacream/src/com/example/couacream/MainActivity.java
+++ chouacream/src/com/example/couacream/MainActivity.java
@@ -4,13 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	Button btn1;
 
 	@Override 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Button btn1 = (Button) findViewById(R.id.btn1);
 	}
 
 	@Override
