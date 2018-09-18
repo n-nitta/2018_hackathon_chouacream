092a55d22791d25561602cb088cfce1fa41e10bd
ryusuke-nakagawa <ryu97161@gmail.com>
2018-09-18 17:13:36 +0900

    main画面

---
+++ chouacream/src/com/example/couacream/CourseSelect.java
@@ -0,0 +1,13 @@
+package com.example.couacream;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CourseSelect extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_select);
+    }
+}
